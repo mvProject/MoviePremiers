6c36c0ac11def2b4250f7f9d80a863f5f73c5078
mvproject <awsom7@gmail.com>
2019-03-21 13:04:07 +0200

    test update feature

--- app/src/main/java/com/mvproject/moviepremiers/MainActivity.kt
+++ app/src/main/java/com/mvproject/moviepremiers/MainActivity.kt
@@ -2,11 +2,13 @@ package com.mvproject.moviepremiers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.loadingview.LoadingDialog
+import jp.misyobun.lib.versionupdater.MSBVersionUpdater
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 
@@ -38,5 +40,15 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Date","onResume")
+        val updater = MSBVersionUpdater(this)
+        updater.endpoint = "https://github.com/mvProject/MoviePremiers/blob/master/app/release/update.json"
+        updater.title = "MoviePremiers Update" // Notice
+        updater.message = "You can update to version! 1.2.0" // Yout can update new version!
+        updater.executeVersionCheck()
     }
 }
